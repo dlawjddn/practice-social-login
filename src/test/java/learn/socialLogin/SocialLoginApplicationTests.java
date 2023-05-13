bbac2c3d0b8df1937a3802aa6bff080f921dff59
dlawjddn <jwl5015@naver.com>
2023-05-14 00:10:09 +0900

    kakao login

---
+++ src/test/java/learn/socialLogin/SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.socialLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
